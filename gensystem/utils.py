521b6443485eac4bd7b9ab630aa870cf65fd73b0
brenj <brenj@mailbox.org>
2015-04-16 15:06:21 -0500

    Move choice functions into utils
    Testable functions that facilitate gensystem should live within
    gensystem and not in the binary.

--- gensystem/utils.py
+++ gensystem/utils.py
@@ -1,4 +1,4 @@
-"""Utilities for dealing with all things Gentoo Linux."""
+"""Utilities for dealing with Gentoo Linux and gensystem."""
 
 import urllib2
 
@@ -20,3 +20,81 @@ def soupify(url_path):
         raise RuntimeError("Could NOT talk to %s." % url_path)
 
     return BeautifulSoup(response.read())
+
+
+def get_choices(items):
+    """Get choices for a given list of items.
+
+    Args:
+        items (list): items to get choices for.
+
+    Returns:
+        dict: choices for the specified items.
+        format: {item1: 1, item2: 2, item3: 3, ...}
+    """
+    return {item: choice for choice, item in enumerate(items, 1)}
+
+
+def get_user_choice(prompt, choices):
+    """Prompt user for a choice and ensure user input is valid.
+
+    Input for a choice will always be presented as an integer. Prompt the
+    user until valid input is received.
+
+    :param prompt: message to prompt the user with
+    :type prompt: str
+    :param valid_choices: valid choices a user can make
+    :type valid_choices: dict
+    :return: valid user choice
+    :rtype: int
+
+    """
+    valid_choices = choices.values()
+
+    while True:
+        try:
+            choice = int(raw_input(prompt))
+            assert choice in valid_choices
+        except (ValueError, AssertionError):
+            continue
+        else:
+            break
+
+    return get_choice_value(choice, choices)
+
+
+def get_choice_value(user_choice, choices):
+    """Get value associated with user choice.
+
+    :param user_choice: valid user choice
+    :type user_choice: int
+    :param choices: valid choices a user can make
+    :type choices: dict
+    :return: choice
+    :rtype: int
+
+    """
+    choice_value = None
+    for name, choice in choices.iteritems():
+        choice_value = name if user_choice == choice else choice_value
+
+    return choice_value
+
+
+def format_choice(choice, choices_per_column=None):
+    """Format a choice so that columns always line up.
+
+    :param choice: a choice (e.g. 1, 2, 3)
+    :type choice: int
+    :param choices_per_column: number of choices in each column
+    :type choices_per_column: int
+    :return: formatted str [ %s] | [%s]
+    :rtype: str
+
+    """
+    if choice < 10 and choices_per_column >= 10:
+        choice_format = "[ %s]"
+    else:
+        choice_format = "[%s]"
+
+    return choice_format % str(choice)
