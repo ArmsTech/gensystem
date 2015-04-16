f47f7f7d4c00a3184c536983e5710674eeb39f2f
brenj <brenj@mailbox.org>
2015-04-16 14:35:25 -0500

    Move arch module into media
    Architecture module should be part of media module.

--- gensystem/media.py
+++ gensystem/media.py
@@ -1,5 +1,6 @@
-"""Manage Gentoo Linux media."""
+"""Gentoo Linux media and related functions."""
 
+from collections import namedtuple
 import os
 import re
 
@@ -11,6 +12,15 @@ GENTOO_MEDIA = {
     'Stage3 (tar.gz)': 'stage3',
     'No-Multi-Lib (tar.gz)': 'nomultilib',
     'Live DVD (iso)': '12.1'}
+
+Arch = namedtuple('Arch', 'name minimal stage3 hardened nomultilib')
+AMD64 = Arch(
+    'amd64',
+    'current-install-amd64-minimal::install-amd64-minimal-\d{8}.iso$',
+    'current-stage3-amd64::stage3-amd64-\d{8}.tar.bz2$',
+    'current-stage3-amd64-hardened::stage3-amd64-hardened-\d{8}.tar.bz2$',
+    'current-stage3-amd64-nomultilib::stage3-amd64-nomultilib-\d{8}.tar.bz2$')
+SUPPORTED_ARCH = {'amd64': AMD64}
 
 
 def get_media_file_url(folder_url, file_regex):
