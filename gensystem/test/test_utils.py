c24a8da96056cfde040a09b7fddcc6bc583f8698
brenj <brenj@mailbox.org>
2015-04-17 17:00:50 -0500

    Break get_choices test apart

--- gensystem/test/test_utils.py
+++ gensystem/test/test_utils.py
@@ -3,13 +3,14 @@
 import gensystem.utils as gensystem_utils
 
 
-def test_get_choices():
-    """Test get_choices."""
-    test_items = ['B', 'A', 'D', 'C']
-
-    # Sorted by default
-    choices = gensystem_utils.get_choices(test_items)
+def test_get_choices_sorted():
+    """Test get_choices with sorting (default)."""
+    choices = gensystem_utils.get_choices(['B', 'A', 'D', 'C'])
     assert choices == {'A': 1, 'B': 2, 'C': 3, 'D': 4}
 
-    unsorted_choices = gensystem_utils.get_choices(test_items, sort=False)
+
+def test_get_choices_not_sorted():
+    """Test get_choices without sorting."""
+    unsorted_choices = gensystem_utils.get_choices(
+        ['B', 'A', 'D', 'C'], sort=False)
     assert unsorted_choices == {'A': 2, 'B': 1, 'C': 4, 'D': 3}
