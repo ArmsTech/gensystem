caa7f5e54e1152f5d3c8323759c40f6f2710ea48
brenj <brenj@mailbox.org>
2015-04-21 16:22:42 -0500

    Add unit tests for soupify

--- gensystem/test/test_utils.py
+++ gensystem/test/test_utils.py
@@ -1,6 +1,28 @@
 """Unit tests for gensystem utils."""
 
+import StringIO
+import urllib2
+
+import mock
+import pytest
+
 import gensystem.utils as gensystem_utils
+
+
+@mock.patch(
+    'urllib2.urlopen', lambda url: StringIO.StringIO('<b>soupified</b>'))
+def test_soupify_sucess():
+    """Test soupify sucessfully soupifies a URL."""
+    soupified = gensystem_utils.soupify('http://!FakeURL.com')
+    assert soupified.text == 'soupified'
+
+
+@mock.patch('urllib2.urlopen')
+def test_soupify_raises_exception_on_failure(m_urlopen):
+    """Test soupify raises an exception when urlopen fails."""
+    m_urlopen.side_effect = urllib2.URLError("Forced URLError.")
+    assert pytest.raises(
+        RuntimeError, gensystem_utils.soupify, ('http://!FakeURL.com',))
 
 
 def test_get_choices_sorted():
