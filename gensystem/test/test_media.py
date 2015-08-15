bc03c0ca0ad6f4de2182f37be386284e455c6dea
brenj <brenj@mailbox.org>
2015-08-14 20:41:19 -0700

    Update unit test for get_media_file_url
    This commit simply updates the unit test for get_media_file_url which
    recently changed. Caught by Travis and coveralls :)

--- gensystem/test/test_media.py
+++ gensystem/test/test_media.py
@@ -8,19 +8,18 @@ import gensystem.media as gensystem_media
 
 
 @mock.patch('gensystem.utils.soupify')
-def test_get_media_file_url_sucess(m_soupify):
+def test_get_media_file_url_success(m_soupify):
     """Test get_media_file_url sucessfully gets a gentoo media URL."""
     fake_soupified = mock.MagicMock()
     fake_soupified.find_all.return_value = [
-        bs4.BeautifulSoup('<a href="test-20151225.tar.bz2">test</a>').a]
+        bs4.BeautifulSoup('<a href="stage3-20151225.tar.bz2">test</a>').a]
     m_soupify.return_value = fake_soupified
 
     media_url = gensystem_media.get_media_file_url(
-        'http://test.com/mirror',
-        'amd64', 'test::test-\d{8}.tar.bz2$')
+        'http://test.com/mirror', 'amd64', 'stage3')
     assert media_url == (
         'http://test.com/mirror/releases/amd64/'
-        'autobuilds/test/test-20151225.tar.bz2')
+        'autobuilds/current-stage3-amd64/stage3-20151225.tar.bz2')
 
 
 @mock.patch('gensystem.utils.soupify')
@@ -33,8 +32,7 @@ def test_get_media_file_url_fail(m_soupify):
     m_soupify.return_value = fake_soupified
     assert pytest.raises(
         RuntimeError, gensystem_media.get_media_file_url,
-        'http://test.com/mirror',
-        'amd64', 'test::test-\d{8}.tar.bz2$')
+        'http://test.com/mirror', 'amd64', 'stage3')
 
     # Something besides a link was somehow captured
     fake_soupified.find_all.return_value = [
@@ -42,5 +40,4 @@ def test_get_media_file_url_fail(m_soupify):
     m_soupify.return_value = fake_soupified
     assert pytest.raises(
         RuntimeError, gensystem_media.get_media_file_url,
-        'http://test.com/mirror',
-        'amd64', 'test::test-\d{8}.tar.bz2$')
+        'http://test.com/mirror', 'amd64', 'stage3')
